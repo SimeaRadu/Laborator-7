df13ba561585477e13ad55ccc7ceb039787ead2a
SimeaRadu <92867635+SimeaRadu@users.noreply.github.com>
2021-11-12 22:15:53 +0200

    Add files via upload

---
+++ Radu.js
@@ -0,0 +1,22 @@
+function myFunction()
+{
+	document.getElementById("Radu").innerHTML = "Frontend developer Simea Radu-Stefan";
+	document.getElementById("Fotografia").innerHTML = "Fotograf";
+	document.getElementById("Foto").innerHTML = "Developer";
+	document.getElementById("Video").innerHTML = "Olimpic";
+	document.getElementById("Realizari").innerHTML = "Realizari";
+	document.getElementById('myImage').src='3.jpg'
+	document.getElementById("Family").style.fontFamily = "Impact,Charcoal,sans-serif";
+	document.getElementById("font").style.color = "blue"
+	document.body.style.background = "url('44.jpg')";
+	document.body.style.backgroundColor = "blue"
+}
+function mouseOver() {
+  document.getElementById("demo").style.color = "red";
+  document.getElementById("demo").innerHTML = 2021-2001;
+}
+
+function mouseOut() {
+  document.getElementById("demo").style.color = "black";
+  document.getElementById("demo").innerHTML = "Vârsta";
+}
